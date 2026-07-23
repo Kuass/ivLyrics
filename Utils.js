71b57ce46a80130e663989eea5f214a6873bbee0
ivLis <me@ivl.is>
2026-07-24 02:32:07 +0900

    6.1.5 RELEASE
    PC 버전 표기를 6.1.5로 동기화하고 Spotify DJ 가사 타임라인을 v6.1.3 동작으로 복구했습니다.

--- Utils.js
+++ Utils.js
@@ -1345,7 +1345,7 @@ const Utils = {
   /**
    * Current version of the ivLyrics app
    */
-  currentVersion: "6.1.4",
+  currentVersion: "6.1.5",
 
   /**
    * Check for updates from remote repository
